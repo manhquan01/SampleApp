f821c2e727cdf10d89694b59a883a6c9a5076b4b
manhquan01 <manhquan1908@gmail.com>
2018-04-25 23:29:03 +0700

    chap7

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,28 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
+
+  def create
+    @user = User.new user_params
+    if @user.save
+      flash[:success] = t("users.new.success")
+      redirect_to @user
+    else
+      render :new
+    end
+  end
+
+  def show
+    @user = User.find_by id: params[:id]
+    return if @user
+    flash[:danger] = t("users.new.danger")
+    redirect_to signup_path
+  end
+
+  private
+    def user_params
+      params.require(:user).permit :name, :mail, :password,
+        :password_confirmation
+    end
 end
